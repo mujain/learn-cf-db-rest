9cf411c1d6f3774b2698c06cc6eb06cf1990b068
Mukul Jain <mujain@mujain-mac.gateway.2wire.net>
2013-11-27 17:00:59 -0800

    add project rest-db structure

---
+++ rest-db/src/main/java/com/clousys/learnrestdb/rest_db/App.java
@@ -0,0 +1,13 @@
+package com.clousys.learnrestdb.rest_db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
